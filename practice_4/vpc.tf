20e6690e5a78569cd0c389f646b6745e64596953
Marco Campos <admuser002marco@gmail.com>
2025-05-09 08:43:58 -0700

    Alias, providers y versioning plus aws vpc

---
+++ practice_4/vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "vpc_virginia" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "VPC_VIRGINIA"
+    Env = "Dev"
+  }
+}
+resource "aws_vpc" "vpc_ohio" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "VPC_OHIO"
+    Env = "Dev"
+  }
+  provider = aws.ohio
+}
