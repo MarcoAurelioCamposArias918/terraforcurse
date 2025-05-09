7f2ae238fd7c75fb7b33c1bfa9bb078d2b6b6c57
Marco Campos <admuser002marco@gmail.com>
2025-05-09 09:33:01 -0700

    Variables advance , public and private subnets

---
+++ practice_6/variable.tf
@@ -0,0 +1,14 @@
+variable "virginia_cidr" {
+
+}
+variable "ohio_cidr" {
+ 
+}
+variable "public_subnet" {
+  description = "CIDR public subnet"
+  type = string
+}
+variable "private_subnet" {
+  description = "CIDR private subnet"
+  type = string
+}
--- practice_6/vpc.tf
+++ practice_6/vpc.tf
@@ -5,6 +5,15 @@ resource "aws_vpc" "vpc_virginia" {
     Env = "Dev"
   }
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  cidr_block = var.public_subnet
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  cidr_block = var.private_subnet
+}
 resource "aws_vpc" "vpc_ohio" {
   cidr_block = var.ohio_cidr
   tags = {
@@ -12,10 +21,4 @@ resource "aws_vpc" "vpc_ohio" {
     Env = "Dev"
   }
   provider = aws.ohio
-}
-variable "virginia_cidr" {
-
-}
-variable "ohio_cidr" {
- 
 }
