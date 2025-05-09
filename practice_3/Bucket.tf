484d54182cf5a6284abfaef1aa6e421c800876a6
Marco Campos <admuser002marco@gmail.com>
2025-05-09 08:17:35 -0700

    S3 bucket practice

--- practice_3/Bucket.tf
+++ practice_3/Bucket.tf
@@ -1,3 +1,13 @@
 resource "aws_s3_bucket" "provedores"{
-    
+    bucket = "provedores-region-${random_string.sufijo.id}"
+    tags = {
+        Owner = "Marco Campos"
+        Environment = "Dev"
+    }
+}
+resource "random_string" "sufijo" {
+length = 8
+special = false
+upper = false
+numeric = false
 }
