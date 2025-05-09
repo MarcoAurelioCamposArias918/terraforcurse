92ab21337568e45e442698d2c17b1c2e8bb1df40
Marco Campos <admuser002marco@gmail.com>
2025-05-09 11:58:10 -0700

    life cycle, provisioners and instance access

--- practice_9/providers.tf
+++ practice_9/providers.tf
@@ -1,21 +1,15 @@
 terraform {
- required_providers {
+  required_providers {
     aws = {
-    source = "hashicorp/aws"
-    version = "4.43.0"
+      source  = "hashicorp/aws"
+      version = ">=4.36.0, <4.47.0, !=4.43.0"
+    }
   }
- }
- required_version = "~>1.3.0"
+  required_version = "~>1.3.0"
 }
+
 provider "aws" {
   region = "us-east-1"
-  default_tags {
-    tags = var.tags
-  }
-}
-provider "aws" {
-  region = "us-east-2"
-  alias = "ohio"
   default_tags {
     tags = var.tags
   }
