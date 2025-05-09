b44077c74e9641a9d2db7c74d17a58d2c165722a
Marco Campos <admuser002marco@gmail.com>
2025-05-09 08:57:31 -0700

    Variables

--- practice_5/vpc.tf
+++ practice_5/vpc.tf
@@ -1,21 +1,15 @@
 resource "aws_vpc" "vpc_virginia" {
-  cidr_block = var.virginia_cidr
+  cidr_block = "10.10.0.0/16"
   tags = {
     Name = "VPC_VIRGINIA"
     Env = "Dev"
   }
 }
 resource "aws_vpc" "vpc_ohio" {
-  cidr_block = var.ohio_cidr
+  cidr_block = "10.10.0.0/16"
   tags = {
     Name = "VPC_OHIO"
     Env = "Dev"
   }
   provider = aws.ohio
-}
-variable "virginia_cidr" {
-
-}
-variable "ohio_cidr" {
- 
 }
